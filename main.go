88aa53a29eac3f8ec99b52199b807ca48a9e526a
sato <ysudqfs@163.com>
2022-09-14 19:38:11 +0800

    add AppleWatchHeartBeat

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+var heartBeatTpl = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<svg baseProfile=\"full\" height=\"150\" version=\"1.1\" viewBox=\"0,0,200,200\" width=\"150\"\n     xmlns=\"http://www.w3.org/2000/svg\" xmlns:ev=\"http://www.w3.org/2001/xml-events\"\n     xmlns:xlink=\"http://www.w3.org/1999/xlink\"><defs />\n    <g transform=\"translate(100 100)\">\n        <path d=\"M92.71,7.27L92.71,7.27c-9.71-9.69-25.46-9.69-35.18,0L50,14.79l-7.54-7.52C32.75-2.42,17-2.42,7.29,7.27v0 c-9.71,9.69-9.71,25.41,0,35.1L50,85l42.71-42.63C102.43,32.68,102.43,16.96,92.71,7.27z\" fill=\"tomato\" stroke=\"#B6BBC1\" stroke-width=\"2\" transform=\"translate(-50 -50)\" />\n        <animateTransform additive=\"sum\" attributeName=\"transform\" dur=\"1.2s\" repeatCount=\"indefinite\" type=\"scale\" values=\"1; 1.5; 1.25; 1.5; 1.5; 1;\" />\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"visible;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;visible;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;visible;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;visible;hidden;hidden;hidden;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;hidden;visible;hidden;hidden;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;hidden;hidden;visible;hidden;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;hidden;hidden;hidden;visible;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;hidden;hidden;hidden;hidden;visible;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden;visible;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden;visible;hidden\" /></text>\n    </g>\n</svg>"
+
+func main() {
+	if len(os.Args) != 3 {
+		os.Stdout.Write([]byte("args error!"))
+		return
+	}
+	values := strings.Split(os.Args[2], ",")
+	f, _ := os.Create("./assert/heart.svg")
+	fmt.Println(fmt.Sprintf(heartBeatTpl, values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9]))
+	_, err := f.Write([]byte(heartBeatTpl))
+	if err != nil {
+		os.Stdout.Write([]byte(err.Error()))
+		return
+	}
+}
