6766d011eb82549e4a584b7a557c4d6528d94cb3
sato <ysudqfs@163.com>
2022-09-15 16:26:54 +0800

    fix: values length

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-var heartBeatTpl = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<svg baseProfile=\"full\" height=\"150\" version=\"1.1\" viewBox=\"0,0,200,200\" width=\"150\"\n     xmlns=\"http://www.w3.org/2000/svg\" xmlns:ev=\"http://www.w3.org/2001/xml-events\"\n     xmlns:xlink=\"http://www.w3.org/1999/xlink\"><defs />\n    <g transform=\"translate(100 100)\">\n        <path d=\"M92.71,7.27L92.71,7.27c-9.71-9.69-25.46-9.69-35.18,0L50,14.79l-7.54-7.52C32.75-2.42,17-2.42,7.29,7.27v0 c-9.71,9.69-9.71,25.41,0,35.1L50,85l42.71-42.63C102.43,32.68,102.43,16.96,92.71,7.27z\" fill=\"tomato\" stroke=\"#B6BBC1\" stroke-width=\"2\" transform=\"translate(-50 -50)\" />\n        <animateTransform additive=\"sum\" attributeName=\"transform\" dur=\"1.2s\" repeatCount=\"indefinite\" type=\"scale\" values=\"1; 1.5; 1.25; 1.5; 1.5; 1;\" />\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"visible;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;visible;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;visible;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;visible;hidden;hidden;hidden;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;hidden;visible;hidden;hidden;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;hidden;hidden;visible;hidden;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;hidden;hidden;hidden;visible;hidden;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;hidden;hidden;hidden;hidden;visible;hidden;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden;visible;hidden;hidden\" /></text>\n        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden;hidden;visible;hidden\" /></text>\n    </g>\n</svg>"
+var heartBeatTpl = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<svg baseProfile=\"full\" height=\"150\" version=\"1.1\" viewBox=\"0,0,200,200\" width=\"150\"\n     xmlns=\"http://www.w3.org/2000/svg\" xmlns:ev=\"http://www.w3.org/2001/xml-events\"\n     xmlns:xlink=\"http://www.w3.org/1999/xlink\"><defs />\n    <g transform=\"translate(100 100)\">\n        <path d=\"M92.71,7.27L92.71,7.27c-9.71-9.69-25.46-9.69-35.18,0L50,14.79l-7.54-7.52C32.75-2.42,17-2.42,7.29,7.27v0 c-9.71,9.69-9.71,25.41,0,35.1L50,85l42.71-42.63C102.43,32.68,102.43,16.96,92.71,7.27z\" fill=\"tomato\" stroke=\"#B6BBC1\" stroke-width=\"2\" transform=\"translate(-50 -50)\" />\n        <animateTransform additive=\"sum\" attributeName=\"transform\" dur=\"1.2s\" repeatCount=\"indefinite\" type=\"scale\" values=\"1; 1.5; 1.25; 1.5; 1.5; 1;\" />\n%s\n    </g>\n</svg>"
 
 func main() {
 	if len(os.Args) != 3 {
@@ -14,8 +14,25 @@ func main() {
 		return
 	}
 	values := strings.Split(os.Args[2], "\n")
+
+	tpl := "        <text fill=\"bisque\" style=\"font-size:25px; font-family:Arial\" transform=\"translate(-12.5 0)\">%s<animate attributeName=\"visibility\" dur=\"12.0s\" keyTimes=\"0;0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0\" repeatCount=\"indefinite\" values=\"%s\" /></text>\n"
 	f, _ := os.Create("assets/heart.svg")
-	_, err := f.Write([]byte(fmt.Sprintf(heartBeatTpl, values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9])))
+
+	value := ""
+	valuesLength := len(values)
+	hiddenSlice := make([]string, valuesLength)
+	for i := 0; i < valuesLength; i++ {
+		hiddenSlice[i] = "hidden"
+	}
+
+	for i := 0; i < valuesLength; i++ {
+		tmpHidden := make([]string, valuesLength)
+		copy(tmpHidden, hiddenSlice)
+		tmpHidden[i] = "visible"
+		tmpHidden = append(tmpHidden, "hidden")
+		value += fmt.Sprintf(tpl, values[i], strings.Join(tmpHidden, ";"))
+	}
+	_, err := f.Write([]byte(fmt.Sprintf(heartBeatTpl, value)))
 	if err != nil {
 		os.Stdout.Write([]byte(err.Error()))
 		return
